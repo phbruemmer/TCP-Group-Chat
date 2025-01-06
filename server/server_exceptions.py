bbb21f91770d201f2f4a442291b29b84110be61c
phbruemmer <phbruemmer@gmail.com>
2025-01-07 00:07:37 +0100

    - Added LobbyError to raise an Exception when no lobby was found.

--- server/server_exceptions.py
+++ server/server_exceptions.py
@@ -2,3 +2,9 @@ class CmdSetError(Exception):
     def __init__(self, error_message):
         self.error_message = error_message
         super().__init__(self.error_message)
+
+
+class LobbyError(Exception):
+    def __init__(self, error_message):
+        self.error_message = error_message
+        super().__init__(self.error_message)
